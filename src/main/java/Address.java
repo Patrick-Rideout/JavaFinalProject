0aa76166c66b5bf5ce413c51a9aa158dc06d797f
Patrick Rideout <prideout2002@gmail.com>
2023-12-11 22:44:46 -0330

    Updated Address Class.

--- src/main/java/Address.java
+++ src/main/java/Address.java
@@ -24,6 +24,42 @@ public class Address {
     }
 
     /**
+     * Retrieves the street information.
+     *
+     * @return The street information as a String.
+     */
+    public String getStreet() {
+        return street;
+    }
+
+    /**
+     * Retrieves the city information.
+     *
+     * @return The city information as a String.
+     */
+    public String getCity() {
+        return city;
+    }
+
+    /**
+     * Retrieves the province information.
+     *
+     * @return The province information as a String.
+     */
+    public String getProvince() {
+        return province;
+    }
+
+    /**
+     * Retrieves the postal code information.
+     *
+     * @return The postal code information as a String.
+     */
+    public String getPostalCode() {
+        return postalCode;
+    }
+
+    /**
      * Gets the formatted address string including street, city, province, and postal code.
      *
      * @return The formatted address string.
